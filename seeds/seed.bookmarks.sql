23c6cb541a98470c926214f12655254c02a05b03
Ryan Kadel <ryan.kadel88@gmail.com>
2020-01-05 13:24:40 -0500

    first commit for CP 15

---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,8 @@
+INSERT INTO bookmarks (title, url, description, rating)
+VALUES
+   ('Thinkful', 'https://www.thinkful.com','Think outside the classroom','5'),
+   ('Google', 'https://www.google.com', 'Google', '4'),
+   ('MDN', 'https://developer.mozilla.org', 'MDN Site', '3')
+;
+
+
